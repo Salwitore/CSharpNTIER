2c05640799cddc3c672cd3e54443bfb846d37b3f
Harun Üst <harunust1@outlook.com>
2022-10-14 11:42:09 +0300

    CSEFCShadowProperty

--- UserService/Data/ContextClasses/UserContext.cs
+++ UserService/Data/ContextClasses/UserContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 using Microsoft.EntityFrameworkCore.Design;
 using Pomelo.EntityFrameworkCore.MySql;
 using System;
@@ -24,6 +25,32 @@ namespace Data
         {   
             //Her User'ın bir departmanı var , Her departmanın birden çok userı olabilir , Users'daki departmentId Foreign key
             modelBuilder.Entity<User>().HasOne<Department>(u => u.Department).WithMany(d => d.Users).HasForeignKey(u => u.DepartmentId);
+
+            //User entitymize CreatedDate ve UpdatedDate shadow propertylerini tanımladık.
+            modelBuilder.Entity<User>().Property<DateTime>("CreatedDate");
+            modelBuilder.Entity<User>().Property<DateTime>("UpdatedDate");
+        }
+        //Her bir entity nesnesinde shadow propertylere manuel olarak erişmemek için 
+        //SaveChanges() metodunu override ederek bu shadow propertylerine değeri otomatik olarak
+        //ayarlayabiliriz.
+        public override int SaveChanges()
+        {
+            var entries = ChangeTracker //herhangi bir entity'de değişiklik veya ekleme olduğunda entryleri ChangeTracker'la tespit edip enrties'e atıyoruz.
+                .Entries()
+                .Where(e => e.State == EntityState.Added || e.State == EntityState.Modified);
+
+            foreach (var entityEntry in entries)
+            {
+                entityEntry.Property("UpdatedDate").CurrentValue = DateTime.Now; //entity'de değişiklik olduğunda UpdateDate shadow property'si güncellenicek
+
+                if (entityEntry.State == EntityState.Added)
+                {
+                    entityEntry.Property("CreatedDate").CurrentValue = DateTime.Now;//entity'de ekleme olduğunda CreatedDate shadow property'si güncellenicek
+                }
+            }
+
+
+            return base.SaveChanges();
         }
     }
 }
